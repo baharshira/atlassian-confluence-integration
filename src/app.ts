ef3371a114717102c4e66b965f4a2aabb66d78b1
baharshira <131886427+baharshira@users.noreply.github.com>
2025-03-21 09:12:30 -0400

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { router as authRoutes } from './routes/authRoutes';
 import { router as confluenceRoutes } from './routes/confluenceRoutes';
-import { EMPTY_STRING } from './consts';
 
 dotenv.config();
 
-export const app = express();
-
 const port = process.env.PORT || 3000;
-const clientSecret = process.env.CLIENT_SECRET || EMPTY_STRING;
+
+export const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
     res.send('Server is running. Navigate to /auth/login to test OAuth.');
 });
 
